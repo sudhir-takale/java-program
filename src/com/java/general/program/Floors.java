9395882136f42ef0d8cced495e42a3144a615dd6
sudhir <sudhirtakale99@gmail.com>
2023-04-25 22:15:22 +0530

    initial commit

---
+++ src/com/java/general/program/Floors.java
@@ -0,0 +1,8 @@
+package com.java.general.program;
+
+public class Floors {
+
+    public static void main(String[] args) {
+        System.out.println("This is the first java program in idead");
+    }
+}
